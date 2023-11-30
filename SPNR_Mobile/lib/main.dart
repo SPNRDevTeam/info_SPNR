e23e39797fb269c0fc2d1d65c11cb9b6cfa8ccf1
Evgenii <isaenko.ea.work@gmail.com>
2023-11-30 21:13:01 +1000

    Upcoming events 90% done

--- SPNR_Mobile/lib/main.dart
+++ SPNR_Mobile/lib/main.dart
@@ -6,100 +6,93 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 Future<List<Event>> fetchEvents(http.Client client) async {
-	final response = await http.get(Uri.parse('https://my-json-server.typicode.com/Evgen1987RUS/test-json/events/')); // TODO: тестовый джей сон поменять на нормальный 
+  final response = await http.get(Uri.parse(
+      'https://my-json-server.typicode.com/Evgen1987RUS/test-json/events/')); // TODO: тестовый джей сон поменять на нормальный
 
   return compute(parseEvents, response.body);
 }
 
 List<Event> parseEvents(String responseBody) {
-  final parsed = (jsonDecode(responseBody) as List).cast<Map<String, dynamic>>();
+  final parsed =
+      (jsonDecode(responseBody) as List).cast<Map<String, dynamic>>();
 
   return parsed.map<Event>((json) => Event.fromJson(json)).toList();
 }
 
 class Event {
-	final String id;
-	final String name;
-	final String description;
-	final String date;
-	final String imgPath;
+  final String id;
+  final String name;
+  final String description;
+  final String date;
+  final String imgPath;
 
-	const Event({
-		required this.id,
-		required this.name,
-		required this.description,
-		required this.date,
-		required this.imgPath,
-	});
+  const Event({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.date,
+    required this.imgPath,
+  });
 
   factory Event.fromJson(Map<String, dynamic> json) {
-      return Event (
-        id: json['id'] as String,
-        name: json['name'] as String,
-        description: json['description'] as String,
-        date: json['date'] as String,
-        imgPath: json['imgPath'] as String,
-      );
+    return Event(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      description: json['description'] as String,
+      date: json['date'] as String,
+      imgPath: json['imgPath'] as String,
+    );
   }
 }
 
-void main()
-	=> runApp(const SPNRApp());
-
+void main() => runApp(const SPNRApp());
 
 class SPNRApp extends StatelessWidget {
-	const SPNRApp({Key? key}) : super(key: key);
+  const SPNRApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'СПНР',
-      theme: ThemeData(
-        primarySwatch: Colors.amber
-        ),
-        home: const HomePage(
-          title: 'Главная страница'
-          ),
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const HomePage(title: 'Главная страница'),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key); // make it require the title as a key value
+  const HomePage({Key? key, required this.title})
+      : super(key: key); // make it require the title as a key value
   final String title;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title)
-        ),
+      appBar: AppBar(title: Text(title)),
       body: Container(
+        color: Colors.black,
         height: 100,
-        color: Colors.amber,
         child: SingleChildScrollView(
           child: FutureBuilder<List<Event>>(
-          future: fetchEvents(http.Client()),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              print('line 83 snapshot failure');
-              return const Center(
-                child: Text('Ошибка'),
-                ); 
-            }
-            else if (snapshot.hasData) {
-              return EventsList(events: snapshot.data!);
-            }
-            else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
+            future: fetchEvents(http.Client()),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                print('line 83 snapshot failure');
+                return const Center(
+                  child: Text('Ошибка'),
+                );
+              } else if (snapshot.hasData) {
+                return EventsList(events: snapshot.data!);
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
         ),
-            ),
       ),
-  );
+    );
   }
 }
 
@@ -109,13 +102,94 @@ class EventsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      itemCount: events.length,
-      itemBuilder: (context, int index) {
-        return Text(events[index].name);
-      }
+    return ListView.separated(
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: events.length,
+        itemBuilder: (context, int index) {
+          print('displayed new event');
+          return EventDescription(event: events[index]);
+        });
+  }
+}
+
+class EventDescription extends StatelessWidget {
+  const EventDescription({super.key, required this.event});
+  final Event event;
+
+  Row timestamptzToText(Event event) {
+    final parsedYearMonth = event.date.split('-');
+    final year = parsedYearMonth[0];
+    var month = parsedYearMonth[1];
+
+    final parsedDayTime = parsedYearMonth[2].split(' ');
+    final day = parsedDayTime[0];
+    final time = parsedDayTime[1];
+
+    switch (month) {
+      case '01':
+        month = 'ЯНВ';
+      case '02':
+        month = 'ФЕВ';
+      case '03':
+        month = 'МАР';
+      case '04':
+        month = 'АПР';
+      case '05':
+        month = 'МАЙ';
+      case '06':
+        month = 'ИЮН';
+      case '07':
+        month = 'ИЮЛ';
+      case '08':
+        month = 'АВГ';
+      case '09':
+        month = 'СЕН';
+      case '10':
+        month = 'ОКТ';
+      case '11':
+        month = 'НОЯ';
+      case '12':
+        month = 'ДЕК';
+    }
+
+    return Row(
+      children: [
+        Column(
+          children: [
+            Text(
+              day,
+              style: TextStyle(fontSize: 40, color: Colors.red),
+            ),
+            Text(
+              month,
+              style: TextStyle(fontSize: 20, color: Colors.red),
+            ),
+          ],
+        ),
+        Text(
+          time, // TODO: check to see if this works all right
+          style: TextStyle(fontSize: 20),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          timestamptzToText(event),
+          Column(
+            children: [
+              Text(event.name),
+              Text(event.description),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }

4d3a3596679ef9f26498c893fb87d6ad91bb6b6b
Le0Bonhart <Le0Bonhart@yandex.ru>
2023-12-07 21:08:15 +1000

    Home controller configured

--- SPNR_Web/Controllers/HomeController.cs
+++ SPNR_Web/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using SPNR_Web.Models.DataBase;
 using SPNR_Web.Utils;
 using SPNR_Web.DataAccess;
+using SPNR_Web.Models.VMs;
 
 namespace SPNR_Web.Controllers
 {
@@ -16,18 +17,39 @@ namespace SPNR_Web.Controllers
             _unit = unit;
         }
 
-        public IActionResult Index(int? id)
+        public IActionResult Index()
         {
-            return View();
+            HomeIndexVM VM = new HomeIndexVM();
+            VM.Events = _unit.EventRepo.ReadAll();
+            VM.News = _unit.NewsRepo.ReadAll().OrderBy(n => n.PublicationTime);
+            return View(VM);
+        }
+        public IActionResult Event(Guid id)
+        {
+            Event? @event = _unit.EventRepo.ReadFirst(u => u.Id == id);
+            if (@event == null) return View(null);
+
+            HomeEventVM VM = new HomeEventVM();
+            VM.Event = @event;
+            VM.Header = _unit.HeaderRepo.ReadFirst(h => h.EventId == id);
+            VM.HeaderLinks = _unit.HeaderLinkRepo.ReadWhere(l => l.HeaderId == VM.Header.Id);
+            VM.Blocks = _unit.BlockRepo.ReadWhere(b => b.EventId == id);
+            VM.SubEvents = _unit.SubEventRepo.ReadWhere(s => s.EventId == id);
+            return View(VM);
         }
         public IActionResult News()
         {
-            return View();
+            IEnumerable<News> news = _unit.NewsRepo.ReadAll().OrderBy(n => n.PublicationTime);
+            return View(news);
         }
-        [SessionAuthFilter]
-        public IActionResult Add() 
+        public IActionResult About() 
         {
             return View();
+        }
+        public IActionResult Media() 
+        {
+            IEnumerable<MediaLink> links = _unit.MediaLinkRepo.ReadAll().OrderBy(l => l.CreationTime);
+            return View(links);
         }
     }
 }

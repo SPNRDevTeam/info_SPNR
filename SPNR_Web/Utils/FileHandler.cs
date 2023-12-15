261800e6956b4bb786eccd10a1fdddf282dcd0e8
krasnov-la <leonid.krasnov.r255@gmail.com>
2023-12-16 03:50:17 +1000

    Implemented FileHandler

---
+++ SPNR_Web/Utils/FileHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace SPNR_Web.Utils
+{
+    public class FileHandler : IFileHandler
+    {
+        readonly string _mediaPath;
+        public FileHandler(IWebHostEnvironment webHost)
+        {
+            _mediaPath = Path.Combine(webHost.WebRootPath, @"media");
+        }
+
+        public void Delete(string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+
+        public string? Save(IFormFile? file)
+        {
+            if (file is null) return null;
+            string fullPath = Path.Combine(_mediaPath, Guid.NewGuid().ToString() + Path.GetExtension(file.FileName));
+
+            using (var fStream = new FileStream(fullPath, FileMode.Create))
+            {
+                file.CopyTo(fStream);
+            }
+
+            return fullPath;
+        }
+    }
+}
---
+++ SPNR_Web/Utils/IFileHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace SPNR_Web.Utils
+{
+    public interface IFileHandler
+    {
+        string? Save(IFormFile? file);
+        void Delete(string path);
+    }
+}

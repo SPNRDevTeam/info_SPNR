7b5168265a9580419f99b8a6340854b93ca8160c
krasnov-la <leonid.krasnov.r255@gmail.com>
2023-12-16 15:45:20 +1000

    full Event CRUD

--- SPNR_Web/Models/DataBase/Event.cs
+++ SPNR_Web/Models/DataBase/Event.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
@@ -9,10 +10,14 @@ namespace SPNR_Web.Models.DataBase
         [Key]
         [JsonIgnore]
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Text { get; set; }
-        public DateTime DateTime { get; set; }
-        public string ImgPath { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Today;
+        [ValidateNever]
+        public string? ImgPath { get; set; }
     }
 }

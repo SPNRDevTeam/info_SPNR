2efc15802fab3336ab677bc19c2de4cf4b283130
krasnov-la <leonid.krasnov.r255@gmail.com>
2023-12-20 15:10:36 +1000

    Added root user

--- SPNR_Web/DataAccess/AppDBContext.cs
+++ SPNR_Web/DataAccess/AppDBContext.cs
@@ -13,6 +13,12 @@ namespace SPNR_Web.DataAccess
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = new("647cc151-245c-4364-b93b-34c8976aa019"),
+                Login = "root_user",
+                Password = "AQAAAAIAAYagAAAAEGc6P7EULkVfkO5MivnITxyl8gk3BeSTSFv9XqisrvUTPiS5T4RF6lzac71k7OvB6Q=="
+            });
             modelBuilder.HasPostgresExtension("uuid-ossp");
             modelBuilder.Entity<User>().HasIndex("Login");
         }
---
+++ SPNR_Web/Migrations/20231220050547_RootUser.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SPNR_Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class RootUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublicationTime",
+                table: "News",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationTime",
+                table: "MediaLinks",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTime",
+                table: "Events",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Login", "Password" },
+                values: new object[] { new Guid("647cc151-245c-4364-b93b-34c8976aa019"), "root_user", "AQAAAAIAAYagAAAAEGc6P7EULkVfkO5MivnITxyl8gk3BeSTSFv9XqisrvUTPiS5T4RF6lzac71k7OvB6Q==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("647cc151-245c-4364-b93b-34c8976aa019"));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublicationTime",
+                table: "News",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationTime",
+                table: "MediaLinks",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTime",
+                table: "Events",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}

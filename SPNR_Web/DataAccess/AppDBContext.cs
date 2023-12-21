d618100f23239e30c1b8834fd26b31486ea9e75f
krasnov-la <leonid.krasnov.r255@gmail.com>
2023-12-21 15:46:00 +1000

    root user changed

--- SPNR_Web/DataAccess/AppDBContext.cs
+++ SPNR_Web/DataAccess/AppDBContext.cs
@@ -13,6 +13,14 @@ namespace SPNR_Web.DataAccess
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<User>().HasData(
+                new User ()
+                {
+                    Id = new Guid("f7c0b3fb-efed-414b-95b8-043efc9e24bc"),
+                    Login = "root_user",
+                    Password = "AQAAAAIAAYagAAAAEAdzXpS+NOjqjUhwet6RqBjMsF84qY1Bu5k6jIDOeE6IVG5wRN6VUwKB+5ubmxt3Vg=="
+                }
+                );
             modelBuilder.HasPostgresExtension("uuid-ossp");
             modelBuilder.Entity<User>().HasIndex("Login");
         }
---
+++ SPNR_Web/Migrations/20231221054437_rootUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SPNR_Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class rootUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Login", "Password" },
+                values: new object[] { new Guid("f7c0b3fb-efed-414b-95b8-043efc9e24bc"), "root_user", "AQAAAAIAAYagAAAAEAdzXpS+NOjqjUhwet6RqBjMsF84qY1Bu5k6jIDOeE6IVG5wRN6VUwKB+5ubmxt3Vg==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("f7c0b3fb-efed-414b-95b8-043efc9e24bc"));
+        }
+    }
+}
